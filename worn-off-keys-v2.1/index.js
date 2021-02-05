b48095ddfe38d89eef04b8511b34cb8bb0799dc6
abisammy <minecraftlegolover@gmail.com>
2021-02-05 18:01:54 +0100

    Discord JS Dynamic Help Menu (2020)

--- worn-off-keys-v2.1/index.js
+++ worn-off-keys-v2.1/index.js
@@ -1,7 +1,7 @@
-require("dotenv");
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const TOKEN = process.env.TOKEN4;
+
+const { TOKEN } = require("./config.json");
 const loadCommands = require("./commands/load-commands");
 
 client.on("ready", () => {
